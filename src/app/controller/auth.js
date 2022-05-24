ef6733dc19a34222fe58e949c9024cd19eabb6f8
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-05-24 17:52:37 +0200

    create user

--- src/app/controller/auth.js
+++ src/app/controller/auth.js
@@ -1,5 +1,15 @@
+const { StatusCodes } = require('http-status-codes')
+const User = require('../model/user')
+const { BadRequestError } = require('../helpers/errors')
+
 const register = async (req, res) => {
-  res.send('Register user')
+  const user = await User.create({ ...req.body })
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    status: StatusCodes.CREATED,
+    message: 'User has been created successfully',
+    data: user,
+  })
 }
 
 const login = async (req, res) => {
