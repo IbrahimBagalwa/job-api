b1d691f5774b760365b985e56e6138a24c67bfa5
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-05-24 15:12:10 +0200

    customise error

---
+++ src/app/helpers/errors/index.js
@@ -0,0 +1,11 @@
+const CustomAPIError = require('./custom-err')
+const UnauthenticatedError = require('./unaAuthenticated')
+const NotFoundError = require('./notfound')
+const BadRequestError = require('./bad-request')
+
+module.exports = {
+  CustomAPIError,
+  UnauthenticatedError,
+  NotFoundError,
+  BadRequestError,
+}
