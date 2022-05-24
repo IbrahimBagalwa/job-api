d394f384b094084e7265f8657a07d24d2612c4cc
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-05-24 15:12:44 +0200

    handler err

---
+++ src/app/middleware/err-handler.js
@@ -0,0 +1,20 @@
+const { StatusCodes } = require('http-status-codes')
+const CustomAPIError = require('../helpers/errors/')
+
+const errorHandler = (err, req, res, next) => {
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({
+      success: err.success,
+      status: err.statusCode,
+      message: err.message,
+    })
+  }
+
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    success: false,
+    status: StatusCodes.INTERNAL_SERVER_ERROR,
+    message: `Something went wrong, Please try again later`,
+  })
+}
+
+module.exports = errorHandler
