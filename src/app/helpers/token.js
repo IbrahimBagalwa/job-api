1bd50daf18af172c2b75b2eb97600fffc7f70133
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-05-25 18:34:43 +0200

    genarate token

--- src/app/helpers/token.js
+++ src/app/helpers/token.js
@@ -1,3 +1,9 @@
 const pkg = require('jsonwebtoken')
+require('dotenv'), config()
 
 const { sign, verify, decode } = pkg
+const { JWT_EXPIRE_IN_HRS, JWT_KEY } = process.env
+const generateToken = (userId) => {
+  const token = sign({ id: userId }, JWT_KEY, { expiresIn: JWT_EXPIRE_IN_HRS })
+  return token
+}
