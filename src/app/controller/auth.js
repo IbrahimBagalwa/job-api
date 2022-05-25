66d1693ddb32657c5b77429f4a52d9a4b5918896
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-05-25 18:53:26 +0200

    update

--- src/app/controller/auth.js
+++ src/app/controller/auth.js
@@ -4,11 +4,13 @@ const { BadRequestError } = require('../helpers/errors')
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
+  const token = user.createJWT() //INSTANCE METHOD
   res.status(StatusCodes.CREATED).json({
     success: true,
     status: StatusCodes.CREATED,
     message: 'User has been created successfully',
     data: user,
+    token,
   })
 }
 
