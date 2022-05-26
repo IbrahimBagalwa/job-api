6b79e0ae1934fe5f236feaa0a459ba163dbffd1d
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-05-26 14:23:45 +0200

    correct all of auth

--- src/app/controller/auth.js
+++ src/app/controller/auth.js
@@ -1,6 +1,6 @@
 const { StatusCodes } = require('http-status-codes')
 const User = require('../model/user')
-const { BadRequestError } = require('../helpers/errors')
+const { BadRequestError, UnauthenticatedError } = require('../helpers/errors')
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
@@ -15,7 +15,30 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-  res.send('login user')
+  const { email, password } = req.body
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password')
+  }
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new UnauthenticatedError(
+      'Invalid credentials, Email or password invalid'
+    )
+  }
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError(
+      'Invalid credentials, Email or password invalid'
+    )
+  }
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({
+    success: true,
+    status: StatusCodes.OK,
+    message: 'Login success',
+    data: user,
+    token,
+  })
 }
 
 module.exports = { register, login }
