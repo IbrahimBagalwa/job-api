073a9aa8565c56152a3cdc3eeab21cc78e1bb683
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-05-29 21:22:04 +0200

    create a job

--- src/app/controller/jobs.js
+++ src/app/controller/jobs.js
@@ -1,17 +1,18 @@
 const { StatusCodes } = require('http-status-codes')
-
+const Job = require('../model/jobs')
+const { BadRequestError, NotFoundError } = require('../helpers/errors')
 const getAllJobs = async (req, res) => {
   res.send('get all jobs')
 }
 const createJob = async (req, res) => {
-  res
-    .status(StatusCodes.OK)
-    .json({
-      success: true,
-      status: StatusCodes.OK,
-      message: 'Job created successfully',
-      data: req.user,
-    })
+  req.body.createdBy = req.user.userId
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    status: StatusCodes.CREATED,
+    message: 'Job created successfully',
+    data: job,
+  })
 }
 const getAllJobById = async (req, res) => {
   res.send('get single job')
