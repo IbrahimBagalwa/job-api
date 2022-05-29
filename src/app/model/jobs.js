92c93a011caeb48512f3bd661768dcc15c585bf1
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-05-29 20:41:40 +0200

    update model job

---
+++ src/app/model/jobs.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const JobSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, 'Please provider the company name'],
+    minlength: 3,
+    maxlength: 50,
+  },
+  position: {
+    type: String,
+    required: [true, 'Please provider the  position'],
+    minlength: 3,
+    maxlength: 100,
+  },
+  status: {
+    type: String,
+    enum: ['interview', 'declined', 'pending'],
+    default: 'pending',
+  },
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Please provide user'],
+  },
+})
+
+module.exports = mongoose.model('Jobs', JobSchema)
