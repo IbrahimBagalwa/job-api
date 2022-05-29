553c18bc7d563126db50830746dce8b6ce4d1ef1
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-05-29 21:22:34 +0200

    update job model

--- src/app/model/jobs.js
+++ src/app/model/jobs.js
@@ -1,28 +1,31 @@
 const mongoose = require('mongoose')
 
-const JobSchema = new mongoose.Schema({
-  company: {
-    type: String,
-    required: [true, 'Please provider the company name'],
-    minlength: 3,
-    maxlength: 50,
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provider the company name'],
+      minlength: 3,
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, 'Please provider the  position'],
+      minlength: 3,
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
   },
-  position: {
-    type: String,
-    required: [true, 'Please provider the  position'],
-    minlength: 3,
-    maxlength: 100,
-  },
-  status: {
-    type: String,
-    enum: ['interview', 'declined', 'pending'],
-    default: 'pending',
-  },
-  createdBy: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User',
-    required: [true, 'Please provide user'],
-  },
-})
+  { timestamps: true }
+)
 
-module.exports = mongoose.model('Jobs', JobSchema)
+module.exports = mongoose.model('Job', JobSchema)
