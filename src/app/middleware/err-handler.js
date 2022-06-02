8ede7b311ea304082f97ab078e4dcf420dfc64ec
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-06-02 13:48:11 +0200

    update handler error

--- src/app/middleware/err-handler.js
+++ src/app/middleware/err-handler.js
@@ -1,21 +1,44 @@
 const { StatusCodes } = require('http-status-codes')
-const { CustomAPIError } = require('../helpers/errors')
+// const { CustomAPIError } = require('../helpers/errors')
 
 const errorHandler = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({
-      success: err.success,
-      status: err.statusCode,
-      message: err.message,
-    })
-  }
-
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+  let customError = {
     success: false,
-    status: StatusCodes.INTERNAL_SERVER_ERROR,
-    message: `Something went wrong, Please try again later`,
-    errors: err,
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || 'Something went wrong, Please try again later',
+  }
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({
+  //     success: err.success,
+  //     status: err.statusCode,
+  //     message: err.message,
+  //   })
+  // }
+  if (err.name === 'ValidationError') {
+    customError.success,
+      (customError.statusCode = StatusCodes.BAD_REQUEST),
+      (customError.message = Object.values(err.errors)
+        .map((item) => item.message)
+        .join(','))
+  }
+  if (err.code && err.code === 11000) {
+    customError.success,
+      (customError.statusCode = StatusCodes.BAD_REQUEST),
+      (customError.message = `Duplicated value entered for ${Object.keys(
+        err.keyValue
+      )} field, Please choose another value`)
+  }
+  return res.status(customError.statusCode).json({
+    success: customError.success,
+    status: customError.statusCode,
+    message: customError.message,
   })
+  // return res.status(customError.statusCode).json({
+  //   success: false,
+  //   status: StatusCodes.INTERNAL_SERVER_ERROR,
+  //   message: `Something went wrong, Please try again later`,
+  //   err,
+  // })
 }
 
 module.exports = errorHandler
